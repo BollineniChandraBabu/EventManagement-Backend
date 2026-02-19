8424ba1790448363c61d92ff523303bfcf58df13
CHANDRA BABU BOLLINENI <45785707+BollineniChandraBabu@users.noreply.github.com>
2026-02-19 23:55:39 +0530

    Create InstagramWebhookController.java

---
+++ src/main/java/com/familywishes/controller/InstagramWebhookController.java
@@ -0,0 +1,34 @@
+package com.familywishes.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/webhook")
+public class InstagramWebhookController {
+
+    private static final String VERIFY_TOKEN = "chandoo_verify_token";
+
+    @GetMapping
+    public ResponseEntity<String> verifyWebhook(
+            @RequestParam("hub.mode") String mode,
+            @RequestParam("hub.challenge") String challenge,
+            @RequestParam("hub.verify_token") String token) {
+
+        System.out.println("INSTA token: " +token);
+        if ("subscribe".equals(mode) && VERIFY_TOKEN.equals(token)) {
+            return ResponseEntity.ok(challenge);
+        }
+
+        return ResponseEntity.status(403).body("Verification failed");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> receiveWebhook(@RequestBody String payload) {
+
+        System.out.println("Instagram Webhook Event:");
+        System.out.println(payload);
+
+        return ResponseEntity.ok("EVENT_RECEIVED");
+    }
+}
